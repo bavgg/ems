4a182709c517788d10ef198ad50faecb5b7211cb
bavgg <gestopajonas@gmail.com>
2024-09-24 14:10:46 +0800

    fx:includes

--- src/main/java/com/jonasgestopa/ems/MainController.java
+++ src/main/java/com/jonasgestopa/ems/MainController.java
@@ -2,33 +2,32 @@ package com.jonasgestopa.ems;
 
 import com.jonasgestopa.ems.Models.Employee;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+
+import javafx.scene.Node;
+
 import javafx.scene.control.Button;
+
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import java.io.IOException;
 
 public class MainController {
 
     @FXML
     VBox mainContainer;
+
     @FXML
     Button employeeButton;
-
     @FXML
     Button homeButton;
-
     @FXML
     Button salaryButton;
 
     @FXML
     private TableView<Employee> employeeTableView;
-
     @FXML
     private TableColumn<Employee, Integer> employeeIdColumn;
     @FXML
@@ -50,28 +49,62 @@ public class MainController {
     private TextField searchField;
 
     @FXML
+    private Node salaryNode;
+    @FXML
+    private Node employeeNode;
+    @FXML
+    private Node homeNode;
+
+    @FXML
+    private AnchorPane homeView;
+    @FXML
+    private VBox employeeView;
+    @FXML
+    private HBox salaryView;
+
+    @FXML
     public void initialize() {
 
-        // Switch views
-        handleSwitchViews("home-view");
-        employeeButton.setOnAction(event -> handleSwitchViews("employee-view"));
-        homeButton.setOnAction(event -> handleSwitchViews("home-view"));
-        salaryButton.setOnAction(event -> handleSwitchViews("salary-view"));
+        homeView = (AnchorPane) ((AnchorPane) homeNode).lookup("#homeView");
+        employeeView = (VBox) ((VBox) employeeNode).lookup("#employeeView");
+        salaryView = (HBox) ((HBox) salaryNode).lookup("#salaryView");
 
+        homeView.setVisible(false);
+        homeView.setManaged(false);
+        salaryView.setVisible(true);
+        salaryView.setManaged(true);
+        employeeView.setVisible(false);
+        employeeView.setManaged(false);
+
+        employeeButton.setOnAction(event -> setMain("employee-view"));
+        homeButton.setOnAction(event -> setMain("home-view"));
+        salaryButton.setOnAction(event -> setMain("salary-view"));
     }
 
-    private void handleSwitchViews(String view) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(
-                    MainApplication.class.getResource(String.format("switch-views/%s.fxml", view)));
-            Parent itemView = fxmlLoader.load();
-
-            mainContainer.getChildren().clear();
-            mainContainer.getChildren().add(itemView);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void setMain(String view) {
+        if (view == "employee-view") {
+            homeView.setVisible(false);
+            homeView.setManaged(false);
+            salaryView.setVisible(false);
+            salaryView.setManaged(false);
+            employeeView.setVisible(true);
+            employeeView.setManaged(true);
+        } else if (view == "home-view") {
+            homeView.setVisible(true);
+            homeView.setManaged(true);
+            salaryView.setVisible(false);
+            salaryView.setManaged(false);
+            employeeView.setVisible(false);
+            employeeView.setManaged(false);
+        } else if (view == "salary-view") {
+            homeView.setVisible(false);
+            homeView.setManaged(false);
+            salaryView.setVisible(true);
+            salaryView.setManaged(true);
+            employeeView.setVisible(false);
+            employeeView.setManaged(false);
         }
+
     }
 
 }
